27d8bfa949ec66649912abafa9d95d3e02fdd061
Sidhaant Kadam <sidjkadam-cmyk@users.noreply.github.com>
2025-09-16 12:16:43 -0400

    Deploy: add requirements.txt, configure ALLOWED_HOSTS/CSRF_TRUSTED_ORIGINS/STATIC_ROOT for PythonAnywhere

--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -24,9 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rrj0eg44bv)jxrz@g4!8+in=!8+trye-l-x6fp@m%90qupxo)3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'sidjkadam.pythonanywhere.com']
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://sidjkadam.pythonanywhere.com'
+]
 
 
 # Application definition
@@ -121,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
